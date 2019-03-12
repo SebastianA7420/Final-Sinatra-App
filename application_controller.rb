ee336156c9fdf862738a8d499d9a932e90802d5d
Sebastian Andrade <sebastiana7420@hstat.org>
2019-03-12 16:25:43 +0000

    css changes

--- application_controller.rb
+++ application_controller.rb
@@ -1,11 +1,11 @@
 require 'dotenv/load'
 require 'bundler'
+require 'net/http'
 Bundler.require
 
 require_relative 'models/model.rb'
 
 class ApplicationController < Sinatra::Base
-
   get '/' do
     erb :index
   end
